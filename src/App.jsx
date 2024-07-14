1245da4394702e2a5819e462088a546bc9399fde
Yashwant <yashwant08012005@gmail.com>
2024-07-14 17:07:49 +0530

    fixed issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,43 @@
 import useFetchcontent from "./customHooks/useFetchcontent.jsx"
 import Template from "./Template.jsx"
-import ScrollYProgress from './scrollProgress.jsx'
 import { useRef, useState } from "react"
+import { useScroll, useTransform,motion } from 'framer-motion'
+import beam from './assets/beam.png'
 
 export default function App() {
-    const pokemons = useFetchcontent()
+    const pokemons = useFetchcontent(); 
     const [searchQuery,setSearchQuery] = useState('');
     const targetRef = useRef(null);
+
     const handleChange = (event)=>{
         setSearchQuery(event.target.value.toLowerCase());
     }
 
+    const {scrollYProgress } = useScroll({
+        target :targetRef,
+        offset: ["start start","end end"]
+    })
+    const width = useTransform(scrollYProgress,[0,1],["0%","100%"])
   return (
     <div ref = {targetRef}>
-        <span className = 'fixed z-20 top-12 left-2'>
-            <input className = ' border-[#C58FFF] border-2 outline-none caret-[#C58FFF] px-2 py-1 rounded-sm bg-[#141414] text-neutral-100 poppins-medium' type="text" placeholder="Search Pokemon" onChange = {handleChange} />
-        </span>
+        <div className = 'fixed top-12 w-screen flex justify-center z-10'>
+            <input className = 'border-[#141414] border-2 outline-none caret-[#C58FFF] px-2 py-1 rounded-sm bg-[#141414] text-neutral-100 poppins-medium' type="text" placeholder="Search Pokemon" onChange = {handleChange} />
+        </div>
         <div className = 'relative text-white'>
             {
                 pokemons?.filter(pokemon=> {return pokemon.name.includes(searchQuery)}).map((pokemon,i)=>
                     <Template key = {i} pokemon = {pokemon} index = {i} />
             )}
         </div>
-        <ScrollYProgress ref = {targetRef}/>
+        <motion.div className = 'fixed inset-x-0 h-[0.15rem] z-10 top-6'
+          animate = {{y:['0px','0.5px','-0.5px','0px']}}
+          transition = {{repeat:Infinity}}
+        >
+            <motion.div className = 'relative h-full bg-[#E9FFFF] outline-2 outline outline-offset-0 outline-[#33C8FE] rounded-r-full'
+              style = {{width}}>
+                <img className = 'absolute -top-[1.45rem] -right-1 max-w-32 min-w-32' src = {beam} alt="beam"/>
+            </motion.div>
+        </motion.div>
     </div>
 
   )
--- src/stats.jsx
+++ src/stats.jsx
@@ -23,7 +23,7 @@ export default function Stats({title,clusterCloudOf4,row}) {
   }
   return (
     <div className = 'flex justify-center overflow-hidden bg-[#141414] '>
-      <motion.div className = 'flex justify-center w-full text-[5vw] text-white anton-regular border-b-[0.25px] border-neutral-100/20 '
+      <motion.div className = 'flex justify-center w-full text-[5vh] lg:text-[5vw] text-white anton-regular border-b-[0.25px] border-neutral-100/20 '
         onMouseEnter = {handleMouseEnter}
         onMouseLeave={handleMouseLeave}
       >
@@ -49,10 +49,10 @@ function ClusterCloud({clusterCloudOf4,row}){
   const colors = ['bg-[#BBBDFA] ring-[#BBBDFA]/60','bg-[#C1ED42] ring-[#C1ED42]/60','bg-[#FFFFFF] ring-[#FFFFFF]/60']
   return (
     <div className = 'h-full'>
-      {clusterCloudOf4[0] && <span className = {`poppins-medium text-[0.75vw] ${colors[row]} text-black ring-[0.25vw] border-[0.125vw] text-nowrap border-black absolute px-1 rounded-full top-[30%] left-[5%]` } >{clusterCloudOf4[0]}</span>}
-      {clusterCloudOf4[1] && <span  className = {`poppins-medium text-[0.75vw] ${colors[row]} text-black ring-[0.25vw] border-[0.125vw] text-nowrap border-black absolute px-1 rounded-full top-[10%] right-[30%]`} >{clusterCloudOf4[1]}</span>}
-      {clusterCloudOf4[2] && <span  className = {`poppins-medium text-[0.75vw] ${colors[row]} text-black ring-[0.25vw] border-[0.125vw] text-nowrap border-black absolute px-1 rounded-full bottom-[32%] left-[30%]`} >{clusterCloudOf4[2]}</span>}
-      {clusterCloudOf4[3] &&  <span  className = {`poppins-medium text-[0.75vw] ${colors[row]} text-black ring-[0.25vw] border-[0.125vw] text-nowrap border-black absolute px-1 rounded-full right-[5%] bottom-[10%]`} >{clusterCloudOf4[3]}</span>}
+      {clusterCloudOf4[0] && <span className = {`poppins-medium text-[0.75vw] ${colors[row]} text-black max-md:text-[1.5vw] lg:ring-[0.25vw] lg:border-[0.125vw] ring-[0.25vh] border-[0.125vh] text-nowrap border-black absolute px-1 rounded-full top-[30%] left-[5%]` } >{clusterCloudOf4[0]}</span>}
+      {clusterCloudOf4[1] && <span  className = {`poppins-medium text-[0.75vw] ${colors[row]} text-black max-md:text-[1.5vw] lg:ring-[0.25vw] lg:border-[0.125vw] ring-[0.25vh] border-[0.125vh] text-nowrap border-black absolute px-1 rounded-full top-[10%] right-[30%]`} >{clusterCloudOf4[1]}</span>}
+      {clusterCloudOf4[2] && <span  className = {`poppins-medium text-[0.75vw] ${colors[row]} text-black max-md:text-[1.5vw] lg:ring-[0.25vw] lg:border-[0.125vw] ring-[0.25vh] border-[0.125vh] text-nowrap border-black absolute px-1 rounded-full bottom-[32%] left-[30%]`} >{clusterCloudOf4[2]}</span>}
+      {clusterCloudOf4[3] &&  <span  className = {`poppins-medium text-[0.75vw] ${colors[row]} text-black max-md:text-[1.5vw] lg:ring-[0.25vw] lg:border-[0.125vw] ring-[0.25vh] border-[0.125vh] text-nowrap border-black absolute px-1 rounded-full right-[5%] bottom-[10%]`} >{clusterCloudOf4[3]}</span>}
     </div>
   );
 }
