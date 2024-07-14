a4aba3478abd3b2350a4583d569cc4dc75a5ef41
Yashwant <yashwant08012005@gmail.com>
2024-07-14 14:03:28 +0530

    fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import { useState } from 'react'
-import { motion } from 'framer-motion'
-import  Stats  from './stats.jsx'
-import useFetchcontent from './customHooks/useFetchcontent.jsx'
-function App() {
-  return ( 
-    <></>
+import useFetchcontent from "./customHooks/useFetchcontent.jsx"
+import Template from "./Template.jsx"
+import ScrollYProgress from './scrollProgress.jsx'
+import { useRef, useState } from "react"
+
+export default function App() {
+    const pokemons = useFetchcontent()
+    const [searchQuery,setSearchQuery] = useState('');
+    const targetRef = useRef(null);
+    const handleChange = (event)=>{
+        setSearchQuery(event.target.value.toLowerCase());
+    }
+
+  return (
+    <div ref = {targetRef}>
+        <span className = 'fixed z-20 top-12 left-2'>
+            <input className = ' border-[#C58FFF] border-2 outline-none caret-[#C58FFF] px-2 py-1 rounded-sm bg-[#141414] text-neutral-100 poppins-medium' type="text" placeholder="Search by keyword" onChange = {handleChange} />
+        </span>
+        <div className = 'relative text-white'>
+            {
+                pokemons?.filter(pokemon=> {return pokemon.name.includes(searchQuery)}).map((pokemon,i)=>
+                    <Template key = {i} pokemon = {pokemon} index = {i} />
+            )}
+        </div>
+        <ScrollYProgress ref = {targetRef}/>
+    </div>
+
   )
 }
-
-export default App
--- src/customHooks/useFetchcontent.jsx
+++ src/customHooks/useFetchcontent.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import axios from 'axios'
+
 export default function useFetchcontent() {
 
     const [pokemons,setPokemons] = useState([]);
